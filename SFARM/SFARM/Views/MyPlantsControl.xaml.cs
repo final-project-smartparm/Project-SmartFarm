9c00bcc23b03ad024b066d81db2955ee4cd066d2
leekminxx <lkmm0712@gmail.com>
2024-07-09 11:12:38 +0900

    식물정보창 만들기

--- SFARM/SFARM/Views/MyPlantsControl.xaml.cs
+++ SFARM/SFARM/Views/MyPlantsControl.xaml.cs
@@ -24,5 +24,10 @@ namespace SFARM.Views
         {
             InitializeComponent();
         }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
