00463e0890e22e22b41c5e6008d9bcaf38d6bc0c
leekminxx <lkmm0712@gmail.com>
2024-07-11 10:35:04 +0900

    LED 밝기 수정

---
+++ SFARM1/SFARM/Views/PanelControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SFARM.Views
+{
+    public partial class MyPlantsControl : UserControl
+    {
+        private SerialPort serialPort;
+
+        public MyPlantsControl()
+        {
+            InitializeSerialPort();
+            this.Unloaded += MyPlantsControl_Unloaded;
+        }
+
+        private void InitializeSerialPort()
+        {
+            try
+            {
+                serialPort = new SerialPort("COM3", 9600); // COM3 포트는 아두이노가 연결된 포트로 변경해야 합니다.
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error initializing serial port: {ex.Message}");
+            }
+        }
+
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                int brightness = (int)e.NewValue;
+                serialPort.WriteLine(brightness.ToString());
+            }
+        }
+
+        private void MyPlantsControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
