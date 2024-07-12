09c506df522d3c3f64f5a9a16ada8a9b65991969
yongseok2312 <lyos2312@naver.com>
2024-07-12 10:27:07 +0900

    Create s2323martfarm2.ino

---
+++ smartfarm_bluetooth.v3/s2323martfarm2/s2323martfarm2.ino
@@ -0,0 +1,236 @@
+#include <Wire.h>                     // i2C 통신을 위한 라이브러리
+#include <LiquidCrystal_I2C.h>        // LCD 1602 I2C용 라이브러리
+#include <Adafruit_NeoPixel.h>        // 네오픽셀 라이브러리 
+#include "DHT.h"                      // 온습도 센서 라이브러리
+#include <SoftwareSerial.h>           // 소프트웨어 시리얼 라이브러리
+
+#define DHTPIN A0
+#define DHTTYPE DHT11
+#define JODO_PIN A3
+#define water_PIN A2
+#define RX_PIN 3    // 블루투스 모듈의 RX 핀
+#define TX_PIN 2    // 블루투스 모듈의 TX 핀
+
+SoftwareSerial bluetoothSerial(RX_PIN, TX_PIN); // RX, TX 핀 설정
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_NeoPixel RGB_LED = Adafruit_NeoPixel(12, 9, NEO_GRB);
+LiquidCrystal_I2C lcd(0x27, 16, 2);   // 접근주소: 0x3F or 0x27
+
+int AA = 5;               // 모터A 
+int AB = 4;               // 모터A
+int BA = 6;               // 모터B 
+int BB = 7;               // 모터B 
+
+int fanSpeed = 0;  // 팬 속도 상태 변수
+int brightness = 0;  // 받은 밝기 값 변수
+
+// 급수
+const int relayPin = 11;
+const unsigned long motorOnTime = 5000;  // 모터가 켜져 있을 시간 (밀리초)
+const unsigned long motorOffTime = 5000; // 모터가 꺼져 있을 시간 (밀리초)
+
+unsigned long previousMillis = 0; // 마지막으로 모터 상태가 변경된 시간
+bool motorState = false;          // 모터 상태 (켜짐: true, 꺼짐: false)
+
+int JODO;
+
+float fMax_hum = 0.0;                           // 센서 기준값 선언 및 초기화
+float fMax_temp = 0.0;
+float fMin_temp = 0.0;
+int Soil_moisture_reference = 0;
+
+void setup() {
+  pinMode(relayPin, OUTPUT); // 릴레이 핀을 출력으로 설정
+  digitalWrite(relayPin, LOW); // 릴레이 초기 상태를 꺼짐으로 설정
+
+  // 센서 기준값 설정
+  fMax_temp = 22.0;
+  fMin_temp = 18.0;
+  fanSpeed = 2;
+  Soil_moisture_reference = 50;                // 기준 수분값 설정 0 ~ 100%
+  pinMode(JODO_PIN, INPUT);  // A0 핀을 입력으로 설정
+  
+  Serial.begin(9600);                          // 시리얼 통신 초기화 (USB)
+  bluetoothSerial.begin(9600);                 // 블루투스 시리얼 통신 초기화
+  dht.begin();                                 // DHT 초기화
+   
+  lcd.init();                                  // LCD 초기화
+  lcd.backlight();                             // 백라이트 켜기
+  
+  RGB_LED.begin();                             // Neopixel 초기화
+  RGB_LED.setBrightness(200);                  // RGB_LED 밝기조절
+  RGB_LED.show();
+
+  pinMode(AA, OUTPUT);                         // 모터 핀 설정          
+  pinMode(AB, OUTPUT);
+  pinMode(BA, OUTPUT);
+  pinMode(BB, OUTPUT);
+}
+
+void loop() {
+   if (bluetoothSerial.available()) {
+    char command = bluetoothSerial.read();
+    Serial.print("recived:");
+    Serial.print(command);
+    
+    // 받은 데이터에 따라서 밝기 혹은 팬 속도 설정
+    if (command >= '0' && command <= '9') {
+      brightness = command - '0'; // 문자형 숫자를 정수형 숫자로 변환
+      
+      // Neopixel 밝기 설정
+      int mappedBrightness = map(brightness, 0, 9, 0, 255); // 받은 값 범위를 Neopixel 밝기 범위로 매핑
+      RGB_LED.setBrightness(mappedBrightness); // Neopixel 밝기 설정
+      RGB_LED.show(); // Neopixel 표시
+
+    } else if (command == 'A') {
+      fanSpeed = 1;
+    } else if (command == 'B') {
+      fanSpeed = 2;
+    } else if (command == 'C') {
+      fanSpeed = 3;
+    } 
+   }
+  
+  unsigned long currentMillis = millis();
+
+  if (motorState) {
+    if (currentMillis - previousMillis >= motorOnTime) {
+      motorState = false; // 모터 상태를 꺼짐으로 설정
+      previousMillis = currentMillis; // 현재 시간을 저장
+      digitalWrite(relayPin, LOW); // 릴레이 OFF
+    }
+  } else {
+    if (currentMillis - previousMillis >= motorOffTime) {
+      motorState = true; // 모터 상태를 켜짐으로 설정
+      previousMillis = currentMillis; // 현재 시간을 저장
+      digitalWrite(relayPin, HIGH); // 릴레이 ON
+    }
+  }
+  int waterAmount = analogRead(water_PIN);
+  int waterStatus = (waterAmount == 351) ? "부족" : (waterAmount == 369) ? "여유" : "측정 중";
+  JODO = analogRead(JODO_PIN);       // 주변 광 조도 센서 값 지정 (A0 핀으로 변경)
+  float hum = dht.readHumidity();    // 습도값 받아오기
+  float temp = dht.readTemperature();// 온도값 받아오기
+  int Soil_moisture = analogRead(A1);  // 토양 수분값 받아오기
+  int C_Soil_moisture = map(Soil_moisture, 1023, 0, 0, 100);   // 토양 수분값 0~100으로 변환
+
+  Serial.print("Soil_moisture : ");                            // 백분율로 변환한 값
+  Serial.println(C_Soil_moisture);
+  Serial.print("temp:");
+  Serial.print(dht.readTemperature());
+  Serial.print(",");
+  Serial.print("humid:");
+  Serial.println(dht.readHumidity());
+  Serial.print("Water Level: ");
+  Serial.print(waterAmount);
+  Serial.print(" - ");
+  Serial.println(waterStatus);          // 센서 값을 시리얼 모니터에 출력
+
+  // 시리얼 모니터 및 블루투스 출력
+  String data = "Soil_moisture: " + String(C_Soil_moisture) + ", Temp: " + String(temp) + ", Humid: " + String(hum) + ", 조도: " + String(JODO) + ", Water Level: " + String(waterStatus);
+  Serial.println(data);  // USB 시리얼 모니터 출력
+  bluetoothSerial.println(data);  // 블루투스 시리얼 출력
+
+  String strC_Soil = int2String(C_Soil_moisture);  // 자릿수별 표시 변경
+
+  if (C_Soil_moisture < Soil_moisture_reference)  // 수분 값이 기준 보다 낮을 경우
+  {
+    WarrningLCD();
+  }
+  else
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("T: "); lcd.print((int)temp); lcd.print("C ");
+    lcd.setCursor(8, 0);
+    lcd.print("H: "); lcd.print((int)hum); lcd.print("% ");
+    lcd.setCursor(0, 1);
+    lcd.print("Soil_M: "); lcd.print(strC_Soil); lcd.print("   ");
+  }
+
+  // 온도와 습도에 따른 팬 제어
+  if (temp > fMax_temp) {
+    FanONOFF(fanSpeed); 
+    RGB_Color(RGB_LED.Color(0, 0, 0), 10);
+  } else if (temp < fMin_temp) {
+    fanSpeed = 0; // OFF 상태
+    RGB_Color(RGB_LED.Color(100, 100, 100), 10);
+  } else {
+    fanSpeed = 0; // OFF 상태
+    RGB_Color(RGB_LED.Color(0, 0, 0), 10);
+  }
+
+}
+
+void RGB_Color(uint32_t c, int wait) {  // float c 대신 uint32_t c로 수정
+  for (int i = 0; i < RGB_LED.numPixels(); i++) 
+  {  
+     RGB_LED.setPixelColor(i, c);
+     RGB_LED.show();
+     delay(wait);
+  }
+}
+
+String int2String(int val)
+{
+  String sval = "";
+
+  int val2 = (val / 10) % 10;  // 10의 자리
+  int val3 = (val / 100) % 10; // 100의 자리
+  
+  if (val3 >= 1)
+  {
+    sval = String(val);
+  }
+  else
+  {
+     if (val2 >= 1)
+     {
+      sval = " " + String(val);
+     }
+     else
+     {
+      sval = "  " + String(val);
+     }
+  }
+
+  return sval;
+}
+
+void WarrningLCD() { 
+    lcd.clear();
+    delay(200);
+    lcd.backlight();  // 백라이트 켜기
+    lcd.setCursor(0, 0);  // 0번째, 0라인
+    lcd.print("Not enough water");
+    lcd.setCursor(0, 1);  // 1번째, 1라인
+    lcd.print("supply water!!");
+    delay(200);
+}
+
+void FanONOFF(int OnOff)
+{
+  switch(OnOff){
+    case 0:
+     analogWrite(AA, 0);
+     analogWrite(BA, 0);
+     break;
+    case 1:
+      analogWrite(AA, 70);
+      analogWrite(BA, 70);
+      break;
+    case 2:
+      analogWrite(AA, 150);
+      analogWrite(BA, 150);
+      break;
+    case 3:
+      analogWrite(AA, 254);
+      analogWrite(BA, 254);
+      break;
+    default: // 오타 수정 및 기본 처리
+      analogWrite(AA, 0);
+      analogWrite(BA, 0);
+      break;
+  }
+  
+}
