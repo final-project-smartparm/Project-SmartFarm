b136048af138a0f94fa64827100e500a4dac8605
leekminxx <lkmm0712@gmail.com>
2024-08-16 14:56:34 +0900

    로그인 페이지 구현완료

---
+++ SFARM/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SFARM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzM3NjIzMUAzMjM2MmUzMDJlMzBlV2kzSlFvdjJLS0RVbWV2SC9zcUtSY204K0RIWGtxUmNuc0ZIRElmUVBFPQ==");
+        }
+    }
+}
